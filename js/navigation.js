1f064bb092673ba3c1b0b1fc9b9550023c397d25
David Wire <dwire@rallydev.com>
2014-06-07 17:38:57 -0600

    refactoring credential checking and javascript files

---
+++ js/navigation.js
@@ -0,0 +1,16 @@
+(function() {
+  navigateTo = {
+    login: function() {
+      window.location.href = 'login.html';
+    },
+    warning: function() {
+      window.location.href = 'warning.html';
+    },
+    browser: function() {
+      window.location.href = 'browser.html';
+    },
+    app: function() {
+      window.location.href = 'index.html';
+    }
+  };
+})();
