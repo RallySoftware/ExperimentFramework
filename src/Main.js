1af9eb00d4c184b79f5f315b9b910068f5b16d26
Charles Ferentchak <charles@rallydev.com>
2014-06-07 16:51:40 -0400

    Now can query prefs

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,34 @@
+var preferenceManager;
 $( document ).ready(function(){
-
+  var baseUrl = "https://rally1.rallydev.com/slm/webservice/v2.0/Preference";
+  preferenceManager = 
+  {
+    getPrefs:function(pref)
+    {
+      var deferred = Q.defer();
+      superagent.get(baseUrl)
+      .set('Access-Control-Allow-Origin', 'http://localhost:8000/')
+      .withCredentials()
+      .end(function(e,res){
+        if(e) deferred.reject(e);
+        else if(res.error) deferred.reject(new Error(res.body.message));
+        else deferred.resolve(res.body);
+      });
+      return deferred.promise;
+    },
+    savePrefs:function(pref){
+      var deferred = Q.defer();
+      superagent.post(baseUrl)
+      .send({})
+      .withCredentials()
+      .end(function(e,res){
+        if(e) deferred.reject(e);
+        else if(res.error) deferred.reject(new Error(res.body.message));
+        else deferred.resolve(res.body);
+      });
+      return deferred.promise;
+    }
+    
+  };
+  preferenceManager.getPrefs();
 });
-var globalVar = "ninjas";
