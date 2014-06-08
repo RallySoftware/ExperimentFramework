55954e8ffba346c3e7d4f7ac0a4a02911778f9aa
David Wire <dwire@rallydev.com>
2014-06-07 18:02:19 -0600

    added flag for warning page so it comes up when it should

--- js/warning.js
+++ js/warning.js
@@ -1,12 +1,20 @@
 (function() {
   var onAcceptClick = function() {
+    window.sessionStorage.acceptedWarning = true;
+    _leavePage();
+  };
+
+  var _leavePage = function() {
     if (window.sessionStorage.securityToken) {navigateTo.app();}
     else {navigateTo.login();}
-  };
+  }
 
   window.onload = function () {
     if (!util.isChrome()) {
       navigateTo.browser();
+    }
+    else if (window.sessionStorage.acceptedWarning) {
+      _leavePage();
     }
 
     util.addClickListener('accept', onAcceptClick);
