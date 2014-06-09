77db07197bb27f8b3a69b2b7a55648959f1ea755
Charles Ferentchak <charles@rallydev.com>
2014-06-09 13:48:02 -0400

    Close #2

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,22 @@
-var preferenceManager,profileManager;
+
+var guid = (function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+               .toString(16)
+               .substring(1);
+  }
+  return function() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+           s4() + '-' + s4() + s4() + s4();
+  };
+})();
 
 $( document ).ready(function(){
+  var preferenceManager,profileManager;
   var baseUrl = "https://rally1.rallydev.com/slm/webservice/v2.0/";
   var prefUrl = baseUrl + "preference";
-  var userUrl = baseUrl + "user";
+  var userUrl = baseUrl + "user?fetch=ObjectID,DefaultProject,userprofile";
+  var experimentId;
   preferenceManager = 
   {
     getPrefs:function()
@@ -18,7 +31,7 @@ $( document ).ready(function(){
       });
       return deferred.promise;
     },
-    savePrefs:function(pref){
+    save:function(experimentId,field,value,prefObectId){
       var deferred = Q.defer();
       superagent.post(baseUrl)
       .send({})
@@ -53,18 +66,33 @@ $( document ).ready(function(){
       });
     }
   };
-  var imageUrl = sessionStorage.getItem("imageUrl" , imageUrl);
-  function setImageUrl(imageUrl){
-    var profile = $('#profile');
-    profile[0].src = imageUrl;
-    sessionStorage.setItem("imageUrl" , imageUrl);
-    profile.removeClass('hide');
+  
+  function setUp(){
+    var imageUrl = sessionStorage.getItem("imageUrl" , imageUrl);
+    function setImageUrl(imageUrl){
+      var profile = $('#profile');
+      profile[0].src = imageUrl;
+      sessionStorage.setItem("imageUrl" , imageUrl);
+      profile.removeClass('hide');
+      return new Q();
+    }
+    if(imageUrl){
+      return new Q(setImageUrl(imageUrl));
+    }
+    else{
+      return profileManager.getImageUrl()
+        .then(setImageUrl)
+    }
   }
-  if(imageUrl){
-    setImageUrl(imageUrl);
-  }
-  else{
-
-    profileManager.getImageUrl().then(setImageUrl);
-  }
+  setUp()
+  .then(function(){
+    return new Q(new guid());
+  })
+  .then(function(experimentID){
+    function onBlur(a){ 
+      console.log(a.currentTarget.id,a.currentTarget.value);
+    }
+    $('textarea').blur(onBlur);
+    $('input').blur(onBlur);
+  })
 });
